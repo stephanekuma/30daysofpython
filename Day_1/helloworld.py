6a933a0dc93c2512f42d583b6f5e4de66b3f659e
Stéphane Kuma <kumastephane@gmail.com>
2025-07-23 23:40:48 +0000

    feat: complete day 1 challenge

---
+++ Day_1/helloworld.py
@@ -0,0 +1,46 @@
+# Day 1 - 30DaysOfPython Challenge
+import sys
+import math
+
+# ======> Level 2: 1. Print Python version directly
+print("Python version:", sys.version)
+
+# ======> Level 2: 2. Arithmetic operations directly
+print("Addition:", 3 + 4)
+print("Subtraction:", 3 - 4)
+print("Multiplication:", 3 * 4)
+print("Modulus:", 3 % 4)
+print("Division:", 3 / 4)
+print("Exponential:", 3 ** 4)
+print("Floor Division:", 3 // 4)
+
+# ======> Level 2: 3. Print string literals directly
+print("Your name: John")
+print("Your family name: Doe")
+print("Your country: Togo")
+print("I am enjoying 30 days of python")
+
+# ======> Level 2: 4. Check data types directly
+print("Type of 10:", type(10))
+print("Type of 9.8:", type(9.8))
+print("Type of 3.14:", type(3.14))
+print("Type of 4 - 4j:", type(4 - 4j))
+print("Type of ['Asabeneh', 'Python', 'Finland']:", type(['Asabeneh', 'Python', 'Finland']))
+print("Type of 'John':", type("John"))
+print("Type of 'Doe':", type("Doe"))
+print("Type of 'Togo':", type("Togo"))
+
+
+# ======> Level 3: 1. Data type examples directly
+print("int:", type(123))
+print("float:", type(123.456))
+print("complex:", type(3 + 5j))
+print("str:", type("Python is fun"))
+print("bool:", type(False))
+print("list:", type([1, 2, 3]))
+print("tuple:", type((1, 2, 3)))
+print("set:", type({1, 2, 3}))
+print("dict:", type({"lang": "Python", "level": "Beginner"}))
+
+# ======> Level 3: 2. Euclidean distance between (2, 3) and (10, 8)
+print("Euclidean Distance:", math.sqrt((10 - 2)**2 + (8 - 3)**2))  # Output: 9.433981132056603
---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)                    # addition (+)
+print(3 - 1)                    # subtraction (-)
+print(2 * 3)                    # multiplication (*)
+print(3 / 2)                    # division (/)
+print(3 ** 2)                   # exponentiation (**)
+print(3 % 2)                    # modulus (%)
+print(3 // 2)                   # floor division (//)
